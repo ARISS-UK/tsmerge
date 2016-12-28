6b760f26a7b28a80ed63332d8f4e97d941621786
Phil <phil@philcrump.co.uk>
2016-12-28 18:40:35 +0000

    stats: Remove unused functions.

--- merger_stats.c
+++ merger_stats.c
@@ -22,12 +22,6 @@
 extern rxBuffer_t rxBuffer;
 extern mx_thread_t mx_threads[];
 
-
-/* Converts a hex character to its integer value */
-static char from_hex(char ch) {
-  return isdigit(ch) ? ch - '0' : tolower(ch) - 'a' + 10;
-}
-
 /* Converts an integer value to its hex character*/
 static char to_hex(char code) {
   static char hex[] = "0123456789abcdef";
@@ -45,27 +39,6 @@ static char *url_encode(char *str) {
       *pbuf++ = '+';
     else 
       *pbuf++ = '%', *pbuf++ = to_hex(*pstr >> 4), *pbuf++ = to_hex(*pstr & 15);
-    pstr++;
-  }
-  *pbuf = '\0';
-  return buf;
-}
-
-/* Returns a url-decoded version of str */
-/* IMPORTANT: be sure to free() the returned string after use */
-static char *url_decode(char *str) {
-  char *pstr = str, *buf = malloc(strlen(str) + 1), *pbuf = buf;
-  while (*pstr) {
-    if (*pstr == '%') {
-      if (pstr[1] && pstr[2]) {
-        *pbuf++ = from_hex(pstr[1]) << 4 | from_hex(pstr[2]);
-        pstr += 2;
-      }
-    } else if (*pstr == '+') { 
-      *pbuf++ = ' ';
-    } else {
-      *pbuf++ = *pstr;
-    }
     pstr++;
   }
   *pbuf = '\0';
