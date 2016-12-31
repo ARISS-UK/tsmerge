a7475c0dec5e0b94e397a94b2e5f7dac16d1a6ae
Phil <phil@philcrump.co.uk>
2016-12-31 13:19:02 +0000

    merger_file_feed.c: Fix indentation.

--- merger_file_feed.c
+++ merger_file_feed.c
@@ -29,69 +29,67 @@ void *merger_file_feed(void* arg)
   int r;
   
   /* The main network loop */
-	while(1)
-	{	
-		if(file_viewer.tsenabled || file_viewer.csvenabled)
-		{
-	    p = NULL;
+  while(1)
+  {	
+    if(file_viewer.tsenabled || file_viewer.csvenabled)
+    {
+      p = NULL;
 		    
-	    /* See if there is any data still to send to this viewer */
-			pthread_mutex_lock(&merger.lock);
-			while((p = mx_next(&merger, file_viewer.last_station, file_viewer.last_counter)) != NULL)
-			{
-	    		timestamp = timestamp_ms();
-			  if(file_viewer.tsenabled)
-			  {
-			    /* Append TS segment to TS file */
-		      f = fopen(file_viewer.tsfilename, "a+");
-				  /* Try to send the new data to the viewer */
-				  r = fwrite(p->raw, TS_PACKET_SIZE, 1, f);
-				  if(r <= 0)
-				  {
-					  /* An error has occured. */
-					  fprintf(stderr,"Error saving ts output to file: %d\n", r);
-					  /* Disable file output */
-					  file_viewer.tsenabled = 0;
-					  break;
-				  }
-				  fclose(f);
-				}
-				
-				if(file_viewer.csvenabled)
-				{
-			    /* Append CSV segment description to CSV file */
-				  f = fopen(file_viewer.csvfilename, "a+");
-				  /* Create text [timestamp_ms, station id, station name, station packet counter, packet pcr base, packet pcr extension] */
-			    csvTextSize = snprintf(csvText, 127, "%"PRIu64",%"PRIu32",%s,%"PRIu32",%"PRIu64",%"PRIu16"\n",
-			              timestamp,
-			              p->station,
-			              merger.station[p->station].sid,
-			              p->counter,
-				      p->header.pcr_base,
-                                      p->header.pcr_extension
-                                      
-			    );
-				  /* Try to send the new data to the viewer */
-				  r = fwrite(csvText, csvTextSize, 1, f);
-				  if(r <= 0)
-				  {
-					  /* An error has occured. */
-					  fprintf(stderr,"Error saving csv output to file: %d\n", r);
-					  /* Disable file output */
-					  file_viewer.csvenabled = 0;
-					  break;
-				  }
-				  fclose(f);
-				}
-				
-				/* Update viewer state */
-				file_viewer.last_station = p->station;
-				file_viewer.last_counter = p->counter;
-				file_viewer.timestamp = timestamp;
-			}
-			pthread_mutex_unlock(&merger.lock);
-    }
-		
-		sleep_ms(5);
-	}
+      /* See if there is any data still to send to this viewer */
+      pthread_mutex_lock(&merger.lock);
+      while((p = mx_next(&merger, file_viewer.last_station, file_viewer.last_counter)) != NULL)
+      {
+        timestamp = timestamp_ms();
+        if(file_viewer.tsenabled)
+        {
+          /* Append TS segment to TS file */
+          f = fopen(file_viewer.tsfilename, "a+");
+          /* Try to send the new data to the viewer */
+          r = fwrite(p->raw, TS_PACKET_SIZE, 1, f);
+          if(r <= 0)
+          {
+            /* An error has occured. */
+            fprintf(stderr,"Error saving ts output to file: %d\n", r);
+            /* Disable file output */
+            file_viewer.tsenabled = 0;
+            break;
+          }
+          fclose(f);
+        }
+
+        if(file_viewer.csvenabled)
+        {
+          /* Append CSV segment description to CSV file */
+          f = fopen(file_viewer.csvfilename, "a+");
+          /* Create text [timestamp_ms, station id, station name, station packet counter, packet pcr base, packet pcr extension] */
+          csvTextSize = snprintf(csvText, 127, "%"PRIu64",%"PRIu32",%s,%"PRIu32",%"PRIu64",%"PRIu16"\n",
+            timestamp,
+            p->station,
+            merger.station[p->station].sid,
+            p->counter,
+            p->header.pcr_base,
+            p->header.pcr_extension
+          );
+          /* Try to send the new data to the viewer */
+          r = fwrite(csvText, csvTextSize, 1, f);
+          if(r <= 0)
+          {
+            /* An error has occured. */
+            fprintf(stderr,"Error saving csv output to file: %d\n", r);
+            /* Disable file output */
+            file_viewer.csvenabled = 0;
+            break;
+          }
+          fclose(f);
+        }
+
+        /* Update viewer state */
+        file_viewer.last_station = p->station;
+        file_viewer.last_counter = p->counter;
+        file_viewer.timestamp = timestamp;
+      }
+      pthread_mutex_unlock(&merger.lock);
+    }		
+    sleep_ms(5);
+  }
 }
